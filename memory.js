bd354366c8d9a1c24c22d4316a4399566aaac891
DesireeJoy <desireebradish@gmail.com>
2020-12-13 11:27:26 -0500

    updates happen

--- memory.js
+++ memory.js
@@ -4,10 +4,53 @@ const grid = document.querySelector(".grid");
 const cardTemplate = document.querySelector("#cardTemplate").content;
 const winPopup = document.querySelector(".modal");
 const popupImage = winPopup.querySelector(".modal__pic");
+const changeBtn = document.querySelector(".changeName");
+const changePopup = document.querySelector(".changeNameWhole");
+const gamesWonAmt = document.querySelector(".gamesWon");
+const nameSelector = document.querySelector(".userName");
+const cardSelector = document.querySelector(".grid__card");
+const replayBtn = document.querySelector(".modal__link");
+const changeName = document.querySelector(".changeNameNow");
 
 let card1 = [];
 
+let userObj = {
+  name: "blank",
+  wonGames: 0,
+};
+
 let i = "1";
+
+function updateName() {
+  userObj.name = "Desi";
+  console.log(userObj);
+}
+
+// Reset Cards without losing User Info
+
+function resetCards() {
+  // Find all fields inside the form, and
+  // make an array from them using the Array.from() method
+  const cardsList = Array.from(grid.querySelectorAll(".grid__card"));
+  console.log(cardsList);
+  // Iterate over the resulting array
+  cardsList.forEach((card) => {
+    const flipBtn = card.querySelector(".grid__flip");
+    flipBtn.removeAttribute("disabled");
+    // add the input event handler to each field
+    card.style.backgroundImage = "url('images/cardBack.png')";
+    winPopup.style.display = "none";
+    console.log(card);
+  });
+  card1 = [];
+  let newGamesWon = (userObj.wonGames += 1);
+  gamesWonAmt.textContent = newGamesWon;
+}
+
+// Reset Function
+function refreshPage() {
+  window.location.reload();
+}
 
 function shuffle(array) {
   var currentIndex = array.length,
@@ -32,7 +75,6 @@ function shuffle(array) {
 // Set Cards Up Twice at the beginning
 function setCards() {
   const newArray = initialCards.concat(initialCards);
-  console.log(newArray);
   const initialCards2 = shuffle(newArray);
   initialCards2.forEach((element) => {
     const cardElement = createCard({
@@ -68,9 +110,11 @@ function flipCard(ele) {
     .querySelector(".grid__flip")
     .addEventListener("click", function (evt) {
       evt.preventDefault();
+
       cardButton.disabled = true;
+
       if (card1.includes(cardImage.id)) {
-        winPopup.classList.add("modal_visible");
+        winPopup.style.display = "flex";
         cardImage.style.backgroundImage =
           "url('images/" + cardImage.id + ".jpg')";
         popupImage.src = "images/" + cardImage.id + ".jpg";
@@ -85,5 +129,29 @@ function flipCard(ele) {
 function addCard(card) {
   grid.append(card);
 }
+
+changeName.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  updateName();
+  console.log("working maybe");
+});
+
+replayBtn.addEventListener("click", (evt) => {
+  resetCards();
+});
+
+//Change Name Popup
+
+changeBtn.addEventListener("click", (evt) => {
+  if (evt.target === changeBtn) {
+    changePopup.style.display = "flex";
+  }
+});
+changePopup.addEventListener("click", (evt) => {
+  if (evt.target === changePopup) {
+    changePopup.style.display = "none";
+    console.log("Suicide");
+  }
+});
 
 setCards();
