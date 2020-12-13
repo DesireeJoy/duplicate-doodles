6454a7defdd9762c30e280d9a5de5cbfc34a3475
DesireeJoy <desireebradish@gmail.com>
2020-12-13 15:04:00 -0500

    No more name, no more ending the game on one match

--- memory.js
+++ memory.js
@@ -10,39 +10,25 @@ const gamesWonAmt = document.querySelector(".gamesWon");
 const nameSelector = document.querySelector(".userName");
 const cardSelector = document.querySelector(".grid__card");
 const replayBtn = document.querySelector(".modal__link");
-const changeName = document.querySelector(".changeNameNow");
+
+const nameForm = document.forms.changeNameModal;
+const name = nameForm.elements.newName;
+const changeNameBtn = nameForm.elements.changeNameNow;
 
 let card1 = [];
 
 let userObj = {
-  name: "blank",
+  name: "User",
   wonGames: 0,
 };
 
 let i = "1";
 
-function updateName() {
-  userObj.name = "Desi";
-  console.log(userObj);
-}
-
 // Reset Cards without losing User Info
 
 function resetCards() {
-  // Find all fields inside the form, and
-  // make an array from them using the Array.from() method
-  const cardsList = Array.from(grid.querySelectorAll(".grid__card"));
-  console.log(cardsList);
-  // Iterate over the resulting array
-  cardsList.forEach((card) => {
-    const flipBtn = card.querySelector(".grid__flip");
-    flipBtn.removeAttribute("disabled");
-    // add the input event handler to each field
-    card.style.backgroundImage = "url('images/cardBack.png')";
-    winPopup.style.display = "none";
-    console.log(card);
-  });
-  card1 = [];
+  winPopup.style.display = "none";
+
   let newGamesWon = (userObj.wonGames += 1);
   gamesWonAmt.textContent = newGamesWon;
 }
@@ -93,7 +79,6 @@ function createCard(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   let cloneID = cardElement.querySelector(".grid__card");
   let cloneBtn = cardElement.querySelector(".grid__flip");
-
   cloneID.value = cardData.id;
   cloneID.id = cardData.name;
   flipCard(cardElement);
@@ -110,7 +95,6 @@ function flipCard(ele) {
     .querySelector(".grid__flip")
     .addEventListener("click", function (evt) {
       evt.preventDefault();
-
       cardButton.disabled = true;
 
       if (card1.includes(cardImage.id)) {
@@ -130,28 +114,8 @@ function addCard(card) {
   grid.append(card);
 }
 
-changeName.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  updateName();
-  console.log("working maybe");
-});
-
 replayBtn.addEventListener("click", (evt) => {
   resetCards();
-});
-
-//Change Name Popup
-
-changeBtn.addEventListener("click", (evt) => {
-  if (evt.target === changeBtn) {
-    changePopup.style.display = "flex";
-  }
-});
-changePopup.addEventListener("click", (evt) => {
-  if (evt.target === changePopup) {
-    changePopup.style.display = "none";
-    console.log("Suicide");
-  }
 });
 
 setCards();
